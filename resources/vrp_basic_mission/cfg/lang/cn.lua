ab61136ddf401672caac0198ddb1bc96dbad7a8d
Yueyue01 <yueyue0914@yjjz.com>
2022-05-20 09:51:23 +0800

    任务的UI汉化

---
+++ resources/vrp_basic_mission/cfg/lang/cn.lua
@@ -0,0 +1,57 @@
+
+local lang = {
+  repair = "修理 {1}.",
+  reward = "奖励: {1} $.",
+  bankdriver = "给钱 {1}.",
+  reward = "奖励: {1} $.",
+  --delivery = {
+    --title = "从市场购买物品 (见地图), 然后把它们带给我！",
+    --item = "- {2} {1}"
+  --},
+  drugseller = {
+	title = "找到非法闲人,从他提供的位置收集毒品种子,带他们去毒品实验室(见地图),然后把他们带到我身边！",
+	item = "- {2} {1}"
+	},
+  hacker = {
+	title = "从隐藏终端窃取信用卡然后向我出售信用卡！",
+	item = "- {2} {1}"
+	},
+  fisherman = {
+	title = "去钓鱼(见地图), 然后带上我那些鱼！",
+	item = "- {2} {1}"
+    },
+  weapons_smuggler = {
+	title = "从 Paleto Ammunation 后面获得武器, 然后藏起武器！",
+	item = "- {2} {1}"
+    },
+  medical = {
+	title = "获取毒品 (见地图), 然后将毒品交给我！",
+	item = "- {2} {1}"
+    },
+  -- santa = {
+	-- title = "Deliver the Christmas Presents",
+	-- item = "- {2} {1}"
+    -- },
+  pilot = {
+	title = "收集货物(见地图), 然后将货物交给我！",
+	item = "- {2} {1}"
+    },
+  police = {
+	title = "在警局获得警察报告, 并将报告交给(见地图)  银行经理以显示警察的存在!",
+	item = "- {2} {1}"
+    },
+  ems = {
+	title = "未完善-获取医疗报告(见地图), 然后将报告送到医院",
+	item = "- {2} {1}"
+    },
+  trash = {
+	title = "收集垃圾(见地图) 并将他们带给我！",
+	item = "- {2} {1}"
+    },
+  forger = {
+	title = "从黑客获得银行卡,将他们转换为假身份证 (见地图) 并将他么带给我！",
+	item = "- {2} {1}"
+    }
+}
+
+return lang
